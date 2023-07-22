4e453c110fd91e034b3614460d2bc58698cb46ed
SIVA RAMAN K <59202209+sivaraman2000@users.noreply.github.com>
2023-07-22 13:32:33 +0530

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+// Tic Tac Toe Game by Siva
+public class Main {
+    public static void main(String[] args) { // This is the main function where program starts
+        TicTacToe tictactoe = new TicTacToe();
+    }
+}
